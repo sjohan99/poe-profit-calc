cf3037b441a730ff0ead1ba6fef45946cc647f58
Johan Selin <johanselin99@gmail.com>
2024-09-08 00:47:51 +0200

    Add more items and their sources

--- poe_profit_calc/items.py
+++ poe_profit_calc/items.py
@@ -6,29 +6,156 @@ class PoeNinjaSource(Enum):
     CURRENCY = "Currency"
     UNIQUE_ARMOUR = "UniqueArmour"
     UNIQUE_JEWEL = "UniqueJewel"
+    INVITATION = "Invitation"
+    FRAGMENT = "Fragment"
+    UNIQUE_ACCESSORY = "UniqueAccessory"
+    UNIQUE_FLASK = "UniqueFlask"
+    UNIQUE_WEAPON = "UniqueWeapon"
 
 
 @dataclass
 class Item:
-    """Poe item information.
-
-    poe_ninja_x attributes are used to get the item price from poe.ninja api.
-
-    The URL https://poe.ninja/api/data/currencyoverview?league=Settlers&type=Currency
-    would give 'currencyoverview' as the category and 'Currency' as the type. The name
-    would be found in the
-
-    Attributes:
-        name (int): Item name in pretty form.
-        droprate (str): Chance to drop (0 to 1).
-        poe_ninja_name (str): Item name in poe.ninja api.
-        poe_ninja_category (str): Item category in poe.ninja api.
-        poe_ninja_type (str): Item type in poe.ninja api.
-    """
-
     name: str
     droprate: float
     source: PoeNinjaSource
+
+
+class IncandescentInvitation(Item):
+    name = "Incandescent Invitation"
+    droprate = 0
+    source = PoeNinjaSource.INVITATION
+
+
+class ScreamingInvitation(Item):
+    name = "Screaming Invitation"
+    droprate = 0
+    source = PoeNinjaSource.INVITATION
+
+
+class PolaricInvitation(Item):
+    name = "Polaric Invitation"
+    droprate = 0
+    source = PoeNinjaSource.INVITATION
+
+
+class WrithingInvitation(Item):
+    name = "Writhing Invitation"
+    droprate = 0
+    source = PoeNinjaSource.INVITATION
+
+
+class DevouringFragment(Item):
+    name = "Devouring Fragment"
+    droprate = 0
+    source = PoeNinjaSource.FRAGMENT
+
+
+class RealityFragment(Item):
+    name = "Reality Fragment"
+    droprate = 0
+    source = PoeNinjaSource.FRAGMENT
+
+
+class DecayingFragment(Item):
+    name = "Decaying Fragment"
+    droprate = 0
+    source = PoeNinjaSource.FRAGMENT
+
+
+class BlazingFragment(Item):
+    name = "Blazing Fragment"
+    droprate = 0
+    source = PoeNinjaSource.FRAGMENT
+
+
+class AwakeningFragment(Item):
+    name = "Awakening Fragment"
+    droprate = 0
+    source = PoeNinjaSource.FRAGMENT
+
+
+class CosmicFragment(Item):
+    name = "Cosmic Fragment"
+    droprate = 0
+    source = PoeNinjaSource.FRAGMENT
+
+
+class SynthesisingFragment(Item):
+    name = "Synthesising Fragment"
+    droprate = 0
+    source = PoeNinjaSource.FRAGMENT
+
+
+class FragmentOfEmptiness(Item):
+    name = "Fragment of Emptiness"
+    droprate = 0.5
+    source = PoeNinjaSource.FRAGMENT
+
+
+class FragmentOfTerror(Item):
+    name = "Fragment of Terror"
+    droprate = 0.5
+    source = PoeNinjaSource.FRAGMENT
+
+
+class FragmentOfKnowledge(Item):
+    name = "Fragment of Knowledge"
+    droprate = 0.5
+    source = PoeNinjaSource.FRAGMENT
+
+
+class FragmentOfShape(Item):
+    name = "Fragment of Shape"
+    droprate = 0.5
+    source = PoeNinjaSource.FRAGMENT
+
+
+class FragmentOfConstriction(Item):
+    name = "Fragment of Constriction"
+    droprate = 0
+    source = PoeNinjaSource.FRAGMENT
+
+
+class FragmentOfEnslavement(Item):
+    name = "Fragment of Enslavement"
+    droprate = 0
+    source = PoeNinjaSource.FRAGMENT
+
+
+class FragmentOfEradication(Item):
+    name = "Fragment of Eradication"
+    droprate = 0
+    source = PoeNinjaSource.FRAGMENT
+
+
+class FragmentOfPurification(Item):
+    name = "Fragment of Purification"
+    droprate = 0
+    source = PoeNinjaSource.FRAGMENT
+
+
+class FragmentOfThePhoenix(Item):
+    name = "Fragment of the Phoenix"
+    droprate = 0
+    source = PoeNinjaSource.FRAGMENT
+
+
+class FragmentOfTheHydra(Item):
+    name = "Fragment of the Hydra"
+    droprate = 0
+    source = PoeNinjaSource.FRAGMENT
+
+
+class FragmentOfTheMinotaur(Item):
+    name = "Fragment of the Minotaur"
+    droprate = 0
+    source = PoeNinjaSource.FRAGMENT
+
+
+class FragmentOfTheChimera(Item):
+    name = "Fragment of the Chimera"
+    droprate = 0
+    source = PoeNinjaSource.FRAGMENT
 
 
 class Dawnbreaker(Item):
@@ -77,3 +204,69 @@ class EldritchExaltedOrb(Item):
     name = "Eldritch Exalted Orb"
     droprate = 0.05
     source = PoeNinjaSource.CURRENCY
+
+
+class ShapersTouch(Item):
+    name = "Shaper's Touch"
+    droprate = 0.56
+    source = PoeNinjaSource.UNIQUE_ARMOUR
+
+
+class VoidWalker(Item):
+    name = "Voidwalker"
+    droprate = 0.33
+    source = PoeNinjaSource.UNIQUE_ARMOUR
+
+
+class SolsticeVigil(Item):
+    name = "Solstice Vigil"
+    droprate = 0.1
+    source = PoeNinjaSource.UNIQUE_ACCESSORY
+
+
+class DyingSun(Item):
+    name = "Dying Sun"
+    droprate = 0.01
+    source = PoeNinjaSource.UNIQUE_FLASK
+
+
+class OrbOfDominance(Item):
+    name = "Orb of Dominance"
+    droprate = 0.01
+    source = PoeNinjaSource.CURRENCY
+
+
+class EchoesOfCreation(Item):
+    name = "Echoes of Creation"
+    droprate = 0.46
+    source = PoeNinjaSource.UNIQUE_ARMOUR
+
+
+class TheTidesOfTime(Item):
+    name = "The Tides of Time"
+    droprate = 0.33
+    source = PoeNinjaSource.UNIQUE_ACCESSORY
+
+
+class EntropicDevastation(Item):
+    name = "Entropic Devastation"
+    droprate = 0.20
+    source = PoeNinjaSource.UNIQUE_JEWEL
+
+
+class StarForge(Item):
+    name = "Starforge"
+    droprate = 0.05
+    source = PoeNinjaSource.UNIQUE_WEAPON
+
+
+class SublimeVision(Item):
+    name = "Sublime Vision"
+    droprate = 0.025
+    source = PoeNinjaSource.UNIQUE_JEWEL
+
+
+class CosmicReliquaryKey(Item):
+    name = "Cosmic Reliquary Key"
+    droprate = 0.01
+    source = PoeNinjaSource.FRAGMENT
