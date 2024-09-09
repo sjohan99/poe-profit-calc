74f5580d2a3f99000c14393744bea0eca8512070
Johan Selin <johanselin99@gmail.com>
2024-09-09 17:21:11 +0200

    Add summary endpoint: expected gain from each boss

--- poe_profit_calc/main.py
+++ poe_profit_calc/main.py
@@ -1,3 +1,4 @@
+from cachetools import cached, TTLCache
 from poe_profit_calc.fetcher import FileFetcher, HttpFetcher
 from poe_profit_calc.prices import Prices, fetch_prices
 from poe_profit_calc.items import Item
@@ -8,9 +9,9 @@ import os
 
 
 if os.getenv("POE_RUN_MODE") == "local":
-    prices = Prices(fetcher=FileFetcher(), source_mapping=FILE_PATH_MAPPING)
+    price_fetcher = Prices(fetcher=FileFetcher(), source_mapping=FILE_PATH_MAPPING)
 else:
-    prices = Prices(
+    prices_fetcher = Prices(
         fetcher=HttpFetcher(),
     )
 
@@ -31,6 +32,27 @@ BOSSES = {
     "venarius": Venarius,
     "venarius_uber": VenariusUber,
 }
+
+
+@cached(cache=TTLCache(maxsize=128, ttl=3600))
+def summary():
+    summaries = []
+    for boss in BOSSES.values():
+        prices = price_fetcher.get_prices(boss.drops.union(set(boss.entrance_items)))
+        value = 0
+        awakened_gems = [item for item in boss.drops if "Awakened" in item.name]
+        other_items = [item for item in boss.drops if "Awakened" not in item.name]
+        if awakened_gems:
+            value += sum(prices.get(item, 0) * item.droprate for item in awakened_gems) / len(
+                awakened_gems
+            )
+        value += sum(prices.get(item, 0) * item.droprate for item in other_items)
+        value -= sum(
+            prices.get(item, 0) * quantity for item, quantity in boss.entrance_items.items()
+        )
+        summaries.append({"boss_name": boss.name, "value": value})
+    summaries.sort(key=lambda x: x["value"], reverse=True)
+    return {"bosses": summaries}
 
 
 def format_drop_prices(item_prices: dict[Item, float], drop_items: set[Item]):
@@ -61,7 +83,7 @@ def format_entrance_prices(item_prices: dict[Item, float], entrance_items: dict[
 def get_boss_drops(boss: str):
     boss_data = BOSSES.get(boss, None)
     items = boss_data.drops.union(set(boss_data.entrance_items))
-    item_prices = prices.get_prices(items)
+    item_prices = price_fetcher.get_prices(items)
     return {
         "boss": boss_data.name,
         "drops": format_drop_prices(item_prices, boss_data.drops),
@@ -77,6 +99,11 @@ async def main_route():
     return "Hello, world!"
 
 
-@app.get("/data/{boss}")
+@app.get("/data/boss/{boss}")
 def get_boss_data(boss: str):
     return get_boss_drops(boss)
+
+
+@app.get("/data/summary")
+def get_summary():
+    return summary()
