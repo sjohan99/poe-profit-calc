dbdd4d201fcefb4e55c3ac7bbebb7b381126704f
Johan Selin <johanselin99@gmail.com>
2024-09-29 22:28:20 +0200

    Update page title logic to consider subpaths

--- client/src/components/page-title.tsx
+++ client/src/components/page-title.tsx
@@ -5,15 +5,35 @@ import { CenterContent } from "./center-content";
 
 const routeTitles: Record<string, string> = {
   "/": "Summary",
-  "/bosses": "Bosses",
+  "/boss": "Bossing",
   "/gems": "Gem Levelling",
   "/faq": "Frequency Asked Questions",
   "/about": "About",
 };
 
+/**
+ * Finds the most specific title for a given path by trying all subpaths, starting from the most specific.
+ *
+ * Example:
+ * `"/boss/the_maven"` will try to find a title for `"/boss/the_maven"` and then `"/boss"`.
+ *
+ * @param path the path of the page
+ * @returns The most specific title of the page, if there is one
+ */
+function findTitle(path: string): string {
+  const pathParts = path.split("/");
+  for (let i = pathParts.length; i >= 0; i--) {
+    const subPath = pathParts.slice(0, i).join("/");
+    if (routeTitles[subPath]) {
+      return routeTitles[subPath];
+    }
+  }
+  return "";
+}
+
 export default function PageTitle() {
   const pathName = usePathname();
-  const title = routeTitles[pathName] ?? ""; // Default to 'Summary' if route is not found
+  const title = findTitle(pathName);
 
   return (
     <CenterContent no_pad_children={true}>
